49902083a3835bf937464350b24082ee0d6d7cb4
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-02-13 11:17:56 +0900

    init
    init commit

--- index.php
+++ index.php
@@ -1,1 +1,427 @@
-initaaaaa
+
+<html>
+  <head>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+  </had>
+  <body>
+  <header>
+    <div class="header_box">
+      <img src="<?php echo get_template_directory_uri(); ?>/image/headlogo.png" alt="logo"/ class="logoImage" width="500">
+    </div>
+  </header>
+  <div class="main">
+    <div class="upper_line"></div>
+    <div class="page1_message_1">
+      <span>女性のための発育技術<br>自慢できる美しい髪で明るい生活を楽しめる</span>
+    </div>
+    <div class="under_line"></div>
+
+    <div class="page1_message_2">
+      <span>医師が推奨する技術を<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;使って顧客満足度</span>
+    </div>
+    <span class="page1_message_2_1">98.8%</span>
+
+    <div class="page1_message_4">
+      <div class="page1_message_4_title">
+      <div class="page1_message_4_title_1">
+          <img src="<?php echo get_template_directory_uri(); ?>/image/45312.png" width="40">
+          こんな髪の悩みはありませんか？
+        </div>
+      </div>
+      <ol>
+        <li class="page1_message_4_title_li">
+          <span class="page1_message_4_title_1_1">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/check.png" width="25">
+            薄毛をなんとかごまかそうと髪型に苦心している
+          </span>
+        </li>
+        <li class="page1_message_4_title_li">
+          <span class="page1_message_4_title_1_1">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/check.png" width="25">
+            明るい所や日が当たる場所で、頭髪がみえてしまう...
+          </span>
+        </li>
+        <li class="page1_message_4_title_li">
+        <span class="page1_message_4_title_1_1">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/check.png" width="25">
+            髪が薄くなってきたことにより自信がなくなってた
+          </span>
+        </li>
+        <li class="page1_message_4_title_li">
+        <span class="page1_message_4_title_1_1">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/check.png" width="25">
+            今まで散々色々なものを色々なものを試したがどれも効かなかった
+          </span>
+        </li>
+        <li class="page1_message_4_title_li">
+        <span class="page1_message_4_title_1_1">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/check.png" width="25">
+            風が強いとすぐに髪の毛が乱れてしまう
+          </span>
+        </li>
+      </ol>
+    </div>
+
+<!-- 以下はabsolute -->
+  <div class="page1_message_5">
+    このようなお悩みをお持ちの方
+  </div>
+<!-- 以上はabsolute -->
+
+
+    <div class="page1_message_6">
+      安心してください。当サロンが重ね発見した<br>
+      最新の
+      <span class="page1_message_6_1">育毛発毛促進技術</span>
+      が登場
+    </div>
+
+    <table class="page1_message_6_table">
+      <tr>
+
+        <th>
+          <div class="page2_message_1">
+          <img src="<?php echo get_template_directory_uri(); ?>/image/アセット 1.png" width="300">
+          </div>
+        </th>
+
+        <th>
+          <div class="page2_message_1">
+          <img src="<?php echo get_template_directory_uri(); ?>/image/アセット 2.png" width="300">
+          </div>
+        </th>
+
+        <th>
+          <div class="page2_message_1">
+          <img src="<?php echo get_template_directory_uri(); ?>/image/アセット 3.png" width="300">
+          </div>
+        </th>
+
+      </tr>
+    </table>
+
+    <div class="page2_message_2">
+    <span>ついに
+      <span class="page2_message_2_1">最新の</span>
+      <span class="page2_message_2_2">再生技術</span>
+      で<br><span class="page2_message_2_6"><br></span>
+      <spanc>髪の毛が生える時代</span>
+      がやってきました！<br><span class="page2_message_2_6"><br></span>
+    </span>
+    <span class="page2_message_2_4">
+    当サロンでは希少な臍帯血幹細胞の培養液を使用しており<br><span class="page2_message_2_6"><br></span>
+    </span>
+    <span class="page2_message_2_5">
+      臍帯血由来幹細胞培養液
+    </span>
+      で発毛させます。<span class="page2_message_2_6"><br></span>
+    </div>
+
+
+    <div class="page2_message_3">
+      <sapn>
+      <span class="page2_message_2_6"><br></span>他社の幹細胞培養液は脂肪由来ですが当施術院は<br><span class="page2_message_2_6"><br></span>
+        脂肪由来幹細胞培養液と比較した場合再生因子は<span>264倍も高い濃度で含有しています。</span><br>
+        <span class="page2_message_2_7">(この臍帯血幹細胞の培養液を使用したサロンはRevolvve提供店のみとなります)</span>
+      </span>
+    </div>
+    
+    <div class="page2_message_3_title">
+      <div class="page2_message_3_title_line">&nbsp;</div>
+      POINT
+      <div class="page2_message_3_title_line">&nbsp;</div>
+    </div>
+    
+
+    <div class="page2_message_3">
+      <div class="page2_message_3_1">
+        <ul>
+          <li class="page2_message_3_1_1">
+              薬に依存しないLED照射で細胞事態を活性化!
+          </li>
+        <ul>
+
+
+        <!-- <div class="page2_message_3_1_1">
+          <img src="<?php echo get_template_directory_uri(); ?>/image/17440.png" width="40">
+        </div>
+        <div class="page2_message_3_1_2">
+          薬に依存しないLED照射で細胞事態を活性化!
+        </div> -->
+      </div>
+      <div class="page2_message_3_image">
+      <div class="page2_message_3_image_1"><img src="<?php echo get_template_directory_uri(); ?>/image/LED.jpg" width="400"></div>
+      </div>
+
+      <!-- 以下はabsolute -->
+      <div class="page2_message_3_abusolute">
+        <span class="page2_message_3_abusolute_1">
+        当サロンでは乾燥棒の培養液と合わせてLED照射をおこないます。<br>
+        </span><br>
+        薬などで強制的に発毛させるのではなく、LEDとヒト幹細胞培養液とを<br>
+        組み合わせる事によって
+        <span class="page2_message_3_abusolute_2">
+          髪の毛を作る細胞事態を再生させる事で<br>薄毛を改善させる最先端の発毛システム
+        </span>
+        です。
+      </div>
+      <!-- 以上はabsolute -->
+    </div>
+
+
+    <div class="page2_message_4">
+      <span><img src="<?php echo get_template_directory_uri(); ?>/image/17440.png" width="40">イオン導入の約60倍のエレクトロポーションで効果・持続性がアップ!</span>
+      <span>そもそもヒト幹細胞培養液に限らず、どれだけ高級な美容液や育毛剤を<br>ただ塗っているだけでは表面で止まっているだけで奥には浸透しません。</span>
+        <div class="page2_message_4_image">
+          <div class="page2_message_4_image_1">
+            <span><img src="<?php echo get_template_directory_uri(); ?>/image/スクリーンショット 2021-02-10 13.55.35.png" width="400"></span>
+          </div>
+        </div>
+
+        <!-- 以下はabsolute -->
+        <div class="page2_message_4_abusolute">
+          当サロンではLED治療に加え、エレクトロポーションを使いヒト幹細胞培養液の最高峰である<br>
+          臍帯血由来の培養液を頭皮に浸透させる事で髪の毛を作る毛母細胞を再生させます。<br><br>
+          エレクトロポーションを使えば今まで注射する以外導入することができなかった<br>
+          高分子のものも導入可能になります。イオン導入では難しかった高分子成分やコラーゲンやヒアルロン酸や<br>
+          ヒト幹細胞培養液を浸透させることができます。(浸透力はイオン導入の20~60倍と言われております)<br>また
+          <span>
+            痛みを感じることはありませんので「痛みに弱い」という方や「注射が苦手」な方でも安心です。
+          </span>
+        </div>
+      <!-- 以上はabsolute -->
+    </div>
+
+
+    <div class="page2_message_4">
+      <span><img src="<?php echo get_template_directory_uri(); ?>/image/17440.png" width="40">当店がTV取材を受けました</span>
+        <div class="page2_message_4_image">
+          <span>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/b_aTGKeWGA0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </span>
+        </div>
+    </div>
+
+    <div class="page3_message_box">
+      <div class="page3_message_title">
+        <span>ホームおエージを見た方限定</span>
+      </div>
+      <span>最先端の幹細胞施術による発毛促進</span>
+      <div class="page3_message_1">
+        <span>初回</span>カウンセリング ￥８,０００
+        <span>まずはご相談ください</span>
+        <span><img src="<?php echo get_template_directory_uri(); ?>/image/26526.png" width="40">０６ー７４１０−１９８２</span>
+        <span> ￥２,９８０</span>
+      </div>
+
+      <div class="page3_message_2">
+        初回カウンセリングの流れ
+      </div>
+      <div>01</div>
+      <div class="page3_message_3">
+        専門スタップによるカウンセリング
+      </div>
+      <div>02</div>
+      <div class="page3_message_3">
+        まずは現状をしっかり把握
+      </div>
+      <div>03</div>
+      <div class="page3_message_3">
+        発毛・育毛に徹底したアドバイス
+      </div>
+      <div>04</div>
+      <div class="page3_message_3">
+        今後の状態に三藤氏ができる
+      </div>
+    </div>
+
+    <div class="page3_message_4">
+        あなた自信が<br>薄毛の原因をわかっていますか?
+    </div>
+
+    <div class="page3_message_5">
+    実は、薄毛で悩んでいる方で<br>
+    「自分自身の薄毛の原因」をわじゃっていない方は大多数いらっしゃいます!!
+    </div>
+
+    <div class="page3_message_5_image">
+      <img src="<?php echo get_template_directory_uri(); ?>/image/image-02.jpg" width="400">
+    </div>
+
+    <div class="page3_message_6_title">
+      薄毛の原因は主に2つ。
+    </div>
+    <div class="page3_message_6">
+      １つは「毛髪サイクルの乱れ」でもう1つは「加齢によるコラーゲン等の減少」です。<br><br>
+      髪の毛は2~6年ほどで成長しては抜け落ちてというサイクルを繰り返し、<br>
+      通常毎日100本ぐらいの髪の毛は誰でも抜けています。<br><br>
+      しかしなんらかの原因で毛母細胞の働きが弱くなるってくると<br>
+      次第に細くて柔らかい毛しか生えて来なくなります。<br>
+      その結果薄毛の原因になります。
+    </div>
+
+    <div class="page3_message_6_image">
+      <img src="<?php echo get_template_directory_uri(); ?>/image/image-03.jpg" width="400">  
+    </div>
+
+    <div class="page3_message_6">
+      また頭皮のコラーゲンが不足すると、髪の毛や毛母細胞自身を炒める原因<br>にもなるので、その結果薄毛を誘発してしまいます。
+    </div>
+
+    <div class="page3_message_7">
+    毛母細胞が弱ると<br>薬や従来の施術では難しい...?
+    </div>
+
+    <div class="page3_message_8">
+    毛髪細胞が髪の毛を生成する機能が残っていれば、<br>
+    若々しい髪の毛に戻すために従来通りのアプローチでも有効な場合があります。<br>
+    しかし、毛髪細胞がある一定の水準まで弱ってしまったり、<br>
+    そもそも細胞が死んでしまっていると二度と元の状態に戻すことはできません。<br>
+
+    <span>つまり毛髪細胞が「手遅れ状態」に陥ってしまった方は泣き寝入りするしかなかったのです...</span>
+    </div>
+
+    <div class="page3_message_9">
+    しかし医療の進歩によって「再生医療技術」が進化し、<br>
+    今までになかった新しい毛髪へのアプローチが可能になりました。
+    </div>
+
+
+
+    <div class="page4_message_1">
+      お客様の声
+    </div>
+    <div class="page4_message_1_image">
+      <img src="<?php echo get_template_directory_uri(); ?>/image/image-02.jpg" width="400">
+    </div>
+    <div class="page4_message_2">
+      カウンセリングで悩みの原因や、<br>
+      どうしたら髪の毛が生えてくるのかを丁寧におしえていただきました!
+    </div>
+    <div class="page4_message_3">
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    </div>
+
+
+
+    <div class="page5_message_1">
+      当ページから限定の<br>
+      特別な料金で初回カウンセリングをさせていただきます!
+    </div>
+
+    <div class="page5_message_1_image">
+      <img src="<?php echo get_template_directory_uri(); ?>/image/image-04.jpg" width="400">
+    </div>
+
+    <div class="page5_message_2">
+    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    </div>
+
+    <div class="page6_message_box">
+      <div class="page6_message_1">
+        初回カウンセリングの流れ
+      </div>
+
+      <div class="page6_message_2">
+        <span>01</span>専門スタッフによるカウンセリング
+      </div>
+
+      <div class="page6_message_3">
+        まずは専門スタッフがお客様のお悩みをしっかりお聞きします
+      </div>
+
+      <div class="page6_message_4">
+        <span>02</span>まずは現状をしっかり把握
+      </div>
+
+      <div class="page6_message_3">
+        マイクロスコープを使い、頭皮の現状をしっかりチェック致します
+      </div>
+
+      <div class="page6_message_5">
+        <span>03</span>発毛・育毛に徹底したアドバイス
+      </div>
+
+      <div class="page6_message_3">
+        ご家庭出した方が良いケア、しない方が良いケアをお教え致します
+      </div>
+
+      <div class="page6_message_6">
+        <span>04</span>今後の状態の見通しが出来る
+      </div>
+
+      <div class="page6_message_3">
+        薄くなる原因を徹底的に分析し、カウンセリングが終わる頃には、<br>
+        今抱えているお悩みの原因を解消いたします!!
+      </div>
+
+      <div class="page3_message_title">
+        <span>ホームおエージを見た方限定</span>
+      </div>
+      <span>最先端の幹細胞施術による発毛促進</span>
+      <div class="page3_message_1">
+        <span>初回</span>カウンセリング ￥８,０００
+        <span>まずはご相談ください</span>
+        <span><img src="<?php echo get_template_directory_uri(); ?>/image/26526.png" width="40">０６ー７４１０−１９８２</span>
+        <span> ￥２,９８０</span>
+      </div>
+
+    </div>
+
+    <div class="infomation">Infomation</div>
+    <div class="infomation_image">
+      <img src="<?php echo get_template_directory_uri(); ?>/image/headerlogo.png" alt="logo"/ class="logoImage">
+      <br>
+      本町店の所在地
+    </div>
+
+    <div class="infomation_map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.8817111170133!2d135.4963631152652!3d34.68293479167717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e71d62c8065b%3A0xba12c9e826b33a86!2z44CSNTQxLTAwNTMg5aSn6Ziq5bqc5aSn6Ziq5biC5Lit5aSu5Yy65pys55S677yU5LiB55uu77yU4oiS77yR77yW!5e0!3m2!1sja!2sjp!4v1613025845712!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+
+    <div class="infomation_message_title">
+      店名
+    </div>
+
+    <span>ヒト幹細胞サロン Re'volve本町店</span>
+
+    <div class="infomation_message_title">
+      住所
+    </div>
+
+    <span>大阪府中央区本町４丁目４ー１６ RE-012-080</span>
+
+    <div class="infomation_message_title">
+      電話番号
+    </div>
+
+    <span>06-7410-1982</span>
+
+    <div class="infomation_message_title">
+      メール
+    </div>
+
+    <span>support@revolve-hair.com</span>
+
+    <div class="infomation_message_title">
+      営業時間
+    </div>
+
+    <span>10:00〜20：00(完全予約制)</span>
+
+    <div class="infomation_message_title">
+      定休日
+    </div>
+
+    <span>無休(年末年始12/29〜1/4除く)</span>
+
+    <div class="infomation_message_title">
+      駐車場
+    </div>
+
+    <span>大変申し訳ございません。ご用意しておりません。</span>
+
+
+    </div>
+  </body>
+</html>
